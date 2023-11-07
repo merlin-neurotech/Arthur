84db2c90e2587e6717028b650d5c272344f6191e
jackfaller <85134985+jackfaller@users.noreply.github.com>
2023-11-07 14:39:37 -0500

    Add files via upload

---
+++ ConcentrationSense.py
@@ -0,0 +1,6 @@
+import UnicornPy
+from neurol import streams
+from neurol.connect_device import get_lsl_EEG_inlets
+from neurol.BCI import generic_BCI, automl_BCI
+from neurol import BCI_tools
+from neurol.models import classification_tools
